f526957764a482d29b5c5102a029440c610b1371
Gustavo Araújo <gustavo.avk@msn.com>
2021-09-10 08:47:43 -0300

    Add: Novas Seções
    adicionando seções de Clientes, Serviços e parte do formulário.

---
+++ js/main.js
@@ -0,0 +1,60 @@
+jQuery(document).ready(function($){
+    window.onscroll = function(){
+        if(window.pageYOffset > 140) {
+            $("#header").addClass("active")
+        } else {
+            $("#header").removeClass("active")
+        }
+    }
+
+    //Isotope
+    let btns = $("#servicos .button-group button");
+    btns.click(function(e){
+        $("#servicos .button-group button").removeClass("active");
+        e.target.classList.add("active");
+
+        let selector = $(e.target).attr("data-filter");
+        $("#servicos .grid").isotope({
+            filter: selector,
+        });
+    });
+
+    $(window).on("load", function(){
+        $("#servicos .grid").isotope({
+            filter: "*",
+        });
+    });
+
+    //Magnify
+    $(".grid .popup-link").magnificPopup({
+        type: "image",
+        gallery: {
+            enabled: true,
+            tPrev: "Anterior",
+            tNext: "Próxima",
+            tCounter: "%curr% de %total%",
+        }
+    });
+
+
+    //owl
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 30,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        dots: true,
+        lazyLoad: true,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 1,
+            },1000: {
+                items: 2,
+            },
+        },
+    });
+});
